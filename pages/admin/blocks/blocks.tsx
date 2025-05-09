af054803043ec52987c1c3cdad9fbaa2ac47167d
Claire-Aline Haestie <ca.haestie@gmail.com>
2025-05-09 17:00:11 +0200

    stabilizing block switch position

--- pages/admin/blocks/blocks.tsx
+++ pages/admin/blocks/blocks.tsx
@@ -138,14 +138,17 @@ function Blocs({
         typeof bloc.set_bloc_number === "function"
       ) {
         bloc.set_bloc_number(index + 1);
+        new_bloc_array[index] = await bloc.save_bloc();
       }
-      new_bloc_array.push(bloc);
     });
     // saving the new info in database
-    new_bloc_array.map(async (bloc_to_save: ComponentTypes) => {
-      await bloc_to_save.save_bloc();
+    Promise.all(new_bloc_array).then((data) => {
+      const typedData = data as Array<ComponentTypes>;
+      if (typedData !== undefined && Array.isArray(typedData)) {
+        setBlocs(new_bloc_array);
+        setRefresh(!refresh);
+      }
     });
-    setBlocs(new_bloc_array);
   };
 
   const saveBloc = async (bloc: ComponentTypes) => {
